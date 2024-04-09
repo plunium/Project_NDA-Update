5ce099597790868115d20d5175deca13da55474e
ijayanra <jayanraj.ilangovane@ynov.com>
2024-04-09 15:02:20 +0200

    mise a jours de la camera due a une erreur sur la map level1

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -18,25 +18,13 @@ public class CameraFollow : MonoBehaviour
     }
 
     // Update is called once per frame
-    private void Update()
-    {
+    private void Update(){
         _timer += Time.deltaTime;
         transform.position = Vector3.Lerp(transform.position, Pos[IdPos].position, CameraSpeed);
         transform.rotation = Quaternion.Lerp(transform.rotation, Pos[IdPos].rotation, CameraSpeed);
-
-        if (IdPos >= 0 && IdPos < Pos.Length)
-{
-    transform.position = Vector3.Lerp(transform.position, Pos[IdPos].position, CameraSpeed);
-    transform.rotation = Quaternion.Lerp(transform.rotation, Pos[IdPos].rotation, CameraSpeed);
-}
-else
-{
-    Debug.LogError("IdPos is out of range: " + IdPos);
-}
     }
 
-    public void setRun()
-    {
+    public void setRun(){
         IdPos = 0;
     }
 
